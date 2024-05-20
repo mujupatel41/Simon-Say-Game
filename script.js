1050ab00246c072f9cda30b708a9780db02e8d38
mujupatel41 <mujupatel41@gmail.com>
2024-05-20 11:40:14 +0530

    Flash Button & Level Up

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
 let gameSeq = [];
 let userSeq = [];
 
+let btns = ["gray", "pink", "orange", "blue"];
+
 let started = false;
 let level = 0;
+
+let h2 = document.querySelector("h2");
 
 
 document.addEventListener("keypress", () =>{
     if(started == false){
         console.log("game was started");
         started = true;
+
+        levelUp();
     }
+
+    
 })
+
+function butFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(() => {
+        btn.classList.remove("flash");
+    }, 250);
+}
+
+function levelUp (){
+    level++;
+    h2.innerText = `Level ${level}`;
+
+    let randomIndex = Math.floor(Math.random()*4)
+    let randomColor = btns[randomIndex];
+    let randomBut = document.querySelector(`.${randomColor}`);
+    
+    butFlash(randomBut);
+}
